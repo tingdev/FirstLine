0c3c8ffc91c47ea07d3342d1813525ebaea33c02
kevintu <kevintu@paxsz.com>
2018-09-13 14:37:56 +0800

    added notification

--- app/src/main/java/kevin/com/firstline/MyTitleLayout.java
+++ app/src/main/java/kevin/com/firstline/MyTitleLayout.java
@@ -1,9 +1,13 @@
 package kevin.com.firstline;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.BitmapFactory;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -37,7 +41,15 @@ public class MyTitleLayout extends LinearLayout {
         search.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "search sth?", Toast.LENGTH_SHORT).show();
+                NotificationManager nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification n = new NotificationCompat.Builder(context, null)
+                        .setContentTitle("NotificationTitle")
+                        .setContentText("NotificationText")
+                        .setContentInfo("NotificationInfo")
+                        .setSmallIcon(R.drawable.banana_alpha)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.banana))
+                        .build();
+                nm.notify(1, n);
             }
         });
     }
