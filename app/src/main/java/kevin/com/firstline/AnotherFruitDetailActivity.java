5ebe78f704be21184bccc1f12da6e43c3a5ce746
tingdev <2824822208@qq.com>
2018-11-16 15:09:36 +0800

    should be the normal way to replace findViewById(android.R.id.content) with getWindow().getDecorView()

--- app/src/main/java/kevin/com/firstline/AnotherFruitDetailActivity.java
+++ app/src/main/java/kevin/com/firstline/AnotherFruitDetailActivity.java
@@ -1,5 +1,6 @@
 package kevin.com.firstline;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -21,8 +22,9 @@ public class AnotherFruitDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            findViewById(android.R.id.content).setSystemUiVisibility(
+            getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
         setContentView(R.layout.activity_another_fruit_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.another_toolbar);
